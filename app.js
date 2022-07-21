7384f2eeb1571f97a891eb9eb99d1bfb30726926
pjb2370 <dbwhd634@naver.com>
2022-07-21 18:13:55 +0900

    할 일 수정

---
+++ app.js
@@ -0,0 +1,95 @@
+import express from "express";
+import mysql from "mysql2/promise";
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "sbsst",
+  password: "sbs123414",
+  database: "a9",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+app.get("/todos", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
+  res.json(rows);
+});
+
+app.get("/todos/:id/", async (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+
+  const [rows] = await pool.query(
+    `
+  SELECT *
+  FROM todo
+  WHERE id = ?
+  `,
+    [id]
+  );
+  if (rows.length === 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  res.json(rows[0]);
+});
+
+app.patch("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { perform_date, content } = req.body;
+
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  if (rows.length === 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+  }
+
+  if (!perform_date) {
+    res.status(400).json({
+      msg: "perform_date required",
+    });
+    return;
+  }
+
+  if (!content) {
+    res.status(400).json({
+      msg: "content required",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
+    UPDATE todo
+    SET perform_date = ?,
+    content = ?
+    WHERE id = ?
+    `,
+    [perform_date, content, id]
+  );
+
+  res.json({
+    msg: `${id}번 할일이 수정되었습니다.`,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
